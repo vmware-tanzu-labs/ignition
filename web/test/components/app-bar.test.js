872d292d012d20a085fe495b88f2f2f843c6a319
Joe Fitzgerald <jfitzgerald@pivotal.io>
2018-04-11 01:17:33 -0600

    Remove unused test

--- web/test/components/app-bar.test.js
+++ web/test/components/app-bar.test.js
@@ -2,11 +2,6 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import AppBar from './../../src/components/app-bar'
 
-test('app bar renders', () => {
-  const appBar = shallow(<AppBar />)
-  // expect(appBar.html().includes('Pivotal Ignition')).toBe(true)
-})
-
 test('app bar renders name when the profile is present', () => {
   const profile = {
     Name: 'Test User',
