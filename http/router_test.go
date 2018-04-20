1f93d0bef8c1e3b0ec570ca5b83e0bb15cfd047e
Shawn Neal <sneal@sneal.net>
2018-04-20 15:06:58 -0700

    Remove index router test and rely on default handler

--- http/router_test.go
+++ http/router_test.go
@@ -38,8 +38,6 @@ func testAPI(t *testing.T, when spec.G, it spec.S) {
 	it("creates a valid router", func() {
 		r := api.createRouter()
 		Expect(r).NotTo(BeNil())
-		index := r.GetRoute("index")
-		Expect(index).NotTo(BeNil())
 		assets := r.GetRoute("assets")
 		Expect(assets).NotTo(BeNil())
 		nonexistent := r.GetRoute("nonexistent")
