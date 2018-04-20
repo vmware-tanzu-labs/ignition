8883219b1c05737857f8ac0c306bde3bf43bb61e
Shawn Neal <sneal@sneal.net>
2018-04-20 13:30:23 -0700

    Add initial style and copy
    [#156021599]

--- web/src/components/forbidden.js
+++ web/src/components/forbidden.js
@@ -4,20 +4,37 @@ import classNames from 'classnames';
 import { withStyles } from 'material-ui/styles'
 import Button from 'material-ui/Button'
 
-import milkyWay from './../../images/bkgd_milky-way_full.svg'
+import bkgd403 from './../../images/bkgd_403.svg'
 
 
 const styles = theme => ({
-  body: {
-    fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
-    fontWeight: 'lighter',
-    marginTop: '68px'
-  },
   button: {
+    backgroundColor: '#007D69',
+    color: 'white',
     margin: theme.spacing.unit,
     '&:hover': {
       backgroundColor: '#007363'
     },
+  },
+  forbidden: {
+    backgroundImage: `url("${bkgd403}")`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    height: '100vh',
+    alignItems: 'center',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  text: {
+    fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
+    fontSize: '1.5rem',
+    fontWeight: 'lighter',
+    height: '100vh',
+    width: '80vh',
+    paddingTop: '40px',
+    color: 'white',
+    textAlign: 'center'
   }
 })
 
@@ -26,11 +43,29 @@ class Forbidden extends React.Component {
     super(props)
   }
 
+  handleTryAgainButtonClick = async () => {
+    // TODO: windows.location = login
+  }
+
   render () {
     const { classes } = this.props
     return (
-      <div className={classes.body}>
-        403
+      <div className={classes.forbidden}>
+        <div className={classes.text}>
+          <p>
+            You've attempted to sign in with email@address,
+            which does not grant you access.
+            Please sign in with your company email account.
+          </p>
+          <Button
+            size="large"
+            variant="raised"
+            className={this.props.classes.button}
+            onClick={this.handleTryAgainButtonClick}
+          >
+            Try Again
+          </Button>
+        </div>
       </div>
     )
   }
