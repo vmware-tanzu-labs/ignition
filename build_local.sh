6d2c05f0fcf63dad7925f28ddb3af8bffe1c4a4f
Zac Bergquist <bergquistz@vmware.com>
2021-03-05 08:57:13 -0700

    Update developer tooling
    - Update github org for Concourse pipeline
    - Update build scripts (no more need for GOPATH)
    - Update just enough of the web dependencies to get things building

--- build_local.sh
+++ build_local.sh
@@ -2,23 +2,25 @@
 
 set -e
 
-PROJECT_DIR="$(go env GOPATH)/src/github.com/pivotalservices/ignition"
+PROJECT_DIR=$(dirname $0)
+[[ $PROJECT_DIR == "." ]] && PROJECT_DIR=$(pwd)
+
 BUILD_DIR="$PROJECT_DIR/build"
 
 mkdir -p $BUILD_DIR
 
 pushd $PROJECT_DIR
   go test ./...
-popd
 
-go test ./...
-pushd $PROJECT_DIR/cmd/ignition
-  GOOS=linux GOARCH=amd64 go build -o $BUILD_DIR/ignition-linux
-  GOOS=darwin GOARCH=amd64 go build -o $BUILD_DIR/ignition-mac
-  GOOS=windows GOARCH=amd64 go build -o $BUILD_DIR/ignition-win64
-popd
+  pushd $PROJECT_DIR/cmd/ignition
+    GOOS=linux GOARCH=amd64 go build -o $BUILD_DIR/ignition-linux
+    GOOS=darwin GOARCH=amd64 go build -o $BUILD_DIR/ignition-mac
+    GOOS=windows GOARCH=amd64 go build -o $BUILD_DIR/ignition-win64
+  popd
 
-pushd $PROJECT_DIR/web
-  yarn lint && yarn test && yarn build
-  cp -r dist/* $BUILD_DIR/
+  pushd $PROJECT_DIR/web
+    yarn install && yarn lint && yarn test && yarn build
+    cp -r dist/* $BUILD_DIR/
+  popd
+
 popd
